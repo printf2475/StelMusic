97a961c724a2690447365c19218c2b95960247e9
Jaewo <printf2475@naver.com>
2025-09-19 11:56:32 +0900

    :art: YoutubeApiService DI 추가

--- core/data/src/main/java/com/stelmusic/core/data/di/ApiModule.kt
+++ core/data/src/main/java/com/stelmusic/core/data/di/ApiModule.kt
@@ -1,11 +1,20 @@
 package com.stelmusic.core.data.di
 
+import com.stelmusic.core.data.api.DefaultYoutubeApiService
+import com.stelmusic.core.data.api.YoutubeApiService
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal interface ApiModule {
 
+    @Binds
+    @Singleton
+    fun bindsYoutubeApiService(
+        youtubeApiService: DefaultYoutubeApiService
+    ): YoutubeApiService
 }

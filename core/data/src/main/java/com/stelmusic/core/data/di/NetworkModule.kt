9358c362cbfbf5b0f977cb6ff177df094f5256a6
Jaewo <printf2475@naver.com>
2025-09-19 11:59:38 +0900

    :art: Ktor 관련 DI 추가

--- core/data/src/main/java/com/stelmusic/core/data/di/NetworkModule.kt
+++ core/data/src/main/java/com/stelmusic/core/data/di/NetworkModule.kt
@@ -9,16 +9,12 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.ClientRequestException
 import io.ktor.client.plugins.HttpResponseValidator
-import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.cache.HttpCache
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
-import io.ktor.client.request.accept
 import io.ktor.client.statement.bodyAsText
-import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
@@ -52,14 +48,7 @@ object NetworkModule {
                 ignoreUnknownKeys = true
             })
         }
-        install(HttpTimeout) {
-            requestTimeoutMillis = 15000L
-            connectTimeoutMillis = 15000L
-            socketTimeoutMillis = 15000L
-        }
-        defaultRequest {
-            accept(ContentType.Application.Json)
-        }
+
         HttpResponseValidator {
             handleResponseExceptionWithRequest { exception, _ ->
                 Log.e(TAG, "error: ${exception.message}")
